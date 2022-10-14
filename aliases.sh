f42cd08a142085b357227c5fe30d3506671d6d17
Jose Abel Castellanos Joo <types@Joses-MBP.unm.edu>
2022-10-14 15:33:43 -0600

    Update zsh config files.

--- aliases.sh
+++ aliases.sh
@@ -1,7 +1,6 @@
 # General aliases
-alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
 alias sshLocalUbuntuVM="ssh -p 2222 jose@127.0.0.1"
-alias sshJose='ssh -X jose@10.88.198.99'
+alias sshJose='ssh -X jose@10.88.167.53'
 alias dict='sdcv'
 
 # Directory aliases
@@ -11,10 +10,14 @@ alias papers_for_thesis="cd $PHD_THESIS_DIR/Documents/Papers"
 alias extra="cd $PHD_THESIS_DIR/Documents/Side-Projects/kapur-talks/mpi21"
 #alias report="cd $REPORTS_DIR/$CURRENT_REPORT"
 alias report="cd $SEMINAR_REPORTS/$CURRENT_REPORT"
+alias ta="cd $CURRENT_TA_DIR"
+alias grading="cd $GITHUB_PROJECTS_DIR/cs-241-grading"
 alias thesis="cd $WRITE_UPS_DIR/thesis"
 alias personal_notes="cd $WRITE_UPS_DIR/personal_notes"
-alias seminar="cd $PHD_DOCUMENTS/Seminars/BeihangUniversity-Fall2021"
+alias seminar="cd $SEMINAR"
+alias website="cd $WEBSITE_DIR"
 alias scc="cd $PHD_THESIS_DIR/Documents/Side-Projects/kapur-nsf-proposal"
+alias maxdiff="cd $PHD_THESIS_DIR/Documents/Side-Projects/MaxDiff"
 
 # Program aliases
 alias ocaml="rlwrap ocaml"
@@ -23,24 +26,18 @@ alias m2="M2 --script"
 alias v="vim"
 alias nv="nvim --listen localhost:12345"
 alias nvs="nvim --listen localhost:12345 -S session"
-alias e="emacs -nw"
-alias ec="emacsclient -nw -c"
-alias todo="emacs -nw $PHD_THESIS_DIR/Documents/Org-Files/research_tasks.org"
-alias updatetodos="$SCRIPT_DIR/updateTodoLists.sh"
+alias c="clear"
+alias updatetodos="$HOME/.local/scripts/updateTodoLists.sh"
 alias addref="nvim $PHD_THESIS_DIR/Documents/Write-Ups/references.bib"
 alias smtinterpol="java -jar $APPS_DIR/smtinterpol-2.5-663-gf15aa217.jar"
 alias ccwr="changeCurrentWeeklyReport"
 alias qcu="quickConfigUpdate"
-alias bsptall1="bspLayout tall 1"
 alias tksr="tmux kill-server"
-alias agenda="tmux rename-session agenda;\
-  tmux rename-window -t agenda:1 org-agenda; \
-  tmux send-keys -t agenda:1 \
-  emacsclient\ -nw\ $TODOLIST_DIR/main.org\ \
-  C-m;"
 alias spass="SPASS"
 alias singular="Singular"
 alias mozilla="firefox"
+alias printd="quick_attachment.py 249wdmk85qktm@hpeprint.com "
+alias s="sxiv"
 
 # Docker aliases
 alias seahorn="systemctl start docker && sudo docker run -v $(pwd):/host -it seahorn/seahorn-llvm5"
--- export.sh
+++ export.sh
@@ -2,18 +2,25 @@
 # ZSHRC config
 #export TERM=xterm-256color
 
+export XDG_CONFIG_HOME="$HOME/.config"
 export GPG_TTY="$TTY"
+export PNPM_HOME="/home/jose/.local/share/pnpm"
 export HISTIGNORE='*sudo -S*'
 export ZSH="$HOME/.oh-my-zsh"
 export ZSH_PLUGINS='/usr/local/Cellar'
+export DISABLE_AUTO_UPDATE=true
 export GTK_PATH='/usr/lib/gtk-2.0'
 export _JAVA_AWT_WM_NONREPARENTING=1
 export POWERLEVEL9K_INSTANT_PROMPT=quiet
 
 export APPS_DIR="$HOME/Documents/Apps"
+export EMACS_DIR="$HOME/.config/jose-emacs"
+export EMACS_PROFILE="jose"
 export MATLAB_DIR="$APPS_DIR/MATLAB/R2022a/bin"
 export MSAT_DIR="$APPS_DIR/mathsat-5.6.5-linux-x86_64"
+export GITHUB_ACCOUNT_URL="git@github.com:typesAreSpaces"
 export GITHUB_PROJECTS_DIR="$HOME/Documents/GithubProjects"
+export WEBSITE_DIR="$GITHUB_PROJECTS_DIR/starter-hugo-academic"
 export BOSQUE_DIR="$GITHUB_PROJECTS_DIR/BosqueLanguage"
 export MASTER_THESIS_DIR="$GITHUB_PROJECTS_DIR/master-thesis"
 export PHD_THESIS_DIR="$GITHUB_PROJECTS_DIR/phd-thesis"
@@ -21,6 +28,7 @@ export PHD_SOFTWARE_DIR="$PHD_THESIS_DIR/Software"
 export OUTPUT_DIR="$PHD_SOFTWARE_DIR/Output"
 export PHD_DOCUMENTS="$PHD_THESIS_DIR/Documents"
 export MY_PRESENTATIONS="$PHD_DOCUMENTS/Presentations/Personal"
+export CURRENT_TA_DIR="$PHD_DOCUMENTS/Semesters/Fall/2022/TA-CS-241"
 export TODOLIST_DIR="$PHD_DOCUMENTS/Org-Files"
 export WRITE_UPS_DIR="$PHD_DOCUMENTS/Write-Ups"
 export REPORTS_DIR="$WRITE_UPS_DIR/weekly_reports/Spring/2022"
@@ -36,9 +44,9 @@ export SCRIPT_DIR="$HOME/.local/scripts"
 export ZSH_SCRIPT_DIR="$HOME/.config/zsh/scripts"
 
 export PATH="/usr/bin:$PATH"
+export PATH="$SCRIPT_DIR:$PATH"
 export PATH="/usr/local/opt/llvm/bin:$PATH"
 export PATH="$HOME/.local/bin:$PATH"
-export PATH="$HOME/.local/scripts:$PATH"
 export PATH="$HOME/.opam/system/bin:$PATH"
 export PATH="$HOME/.opam/4.07.0/bin:$PATH"
 export PATH="$HOME/.cargo/bin:$PATH"
