7c914a6c735d4d2a427b0dede98b0b96dd20141b
Jose Abel Castellanos Joo <jabelcastellanosjoo@unm.edu>
2024-03-08 15:32:25 -0700

    Add alias for lazygit.

--- aliases.sh
+++ aliases.sh
@@ -45,6 +45,7 @@ alias mozilla="firefox"
 alias printd="quick_attachment.py 249wdmk85qktm@hpeprint.com "
 alias s="sxiv"
 alias tlmgr="/usr/share/texmf-dist/scripts/texlive/tlmgr.pl --usermode"
+alias lg="lazygit"
 
 # Docker aliases
 alias seahorn="systemctl start docker && sudo docker run -v $(pwd):/host -it seahorn/seahorn-llvm5"
