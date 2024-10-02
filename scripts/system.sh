32326afa3345cc55c776ad3c5722ae15202825a4
Jose Abel Castellanos Joo <jabelcastellanosjoo@unm.edu>
2024-10-02 15:49:01 -0600

    Add fo script to open files using a fzf filter.

--- scripts/system.sh
+++ scripts/system.sh
@@ -43,6 +43,10 @@ se(){
   du -a "$(pwd)" | awk '{ gsub (" ", "\\ ", $0); $1 = ""; print $0; }' | fzf | xargs -r xdg-open; 
 }
 
+fo(){
+  du -a "$(pwd)" | awk '{ gsub (" ", "\\ ", $0); $1 = ""; print $0; }' | xargs -I {} echo {} | fzf --print0 | xargs -0 -o $1
+}
+
 pwdclip(){ 
   pwd | awk '{gsub( " ","\\ " ); print}' | xclip -selection c 
 }
